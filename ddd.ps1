44f49116e7303dd9f104d4feefd58c5af09666be
Valery <valerybun@yahoo.com>
2017-12-26 14:45:36 +0300

    rer
    weewew

--- ddd.ps1
+++ ddd.ps1
@@ -1,4 +1,4 @@
-#$url_vs17 = 'http://tu-server-slv01.corp.waters.com/artifactory/UNIFI-Misc/CITools/VisualStudio/2017/vs_Enterprise.exe'
+$url_vs17 = 'http://tu-server-slv01.corp.waters.com/artifactory/UNIFI-Misc/CITools/VisualStudio/2017/vs_Enterprise.exe'
 #$url_jdk = 'http://tu-server-slv01.corp.waters.com/artifactory/UNIFI-Misc/CITools/jdk-8u151-windows-x64.exe'
 #$url_bamboo = 'http://codedev2.waters.com/bamboo/agentServer/agentInstaller'
 $url_git = "https://github.com/git-for-windows/git/releases/download/v2.15.1.windows.2/Git-2.15.1.2-64-bit.exe"
@@ -6,7 +6,7 @@ $url_git = "https://github.com/git-for-windows/git/releases/download/v2.15.1.win
 #$JavaPath = $($Env:PATH + ';' + 'C:\Java\jdk1.8.0_151\bin')
 #$GitPath = $($Env:PATH + ';' + 'C:\Program Files\Git\bin')
 
-#Invoke-WebRequest -Uri $url_vs17 -OutFile vs_2017.exe 
+Invoke-WebRequest -Uri $url_vs17 -OutFile vs_2017.exe 
 #Invoke-WebRequest -Uri $url_jdk -OutFile jdk-8u151-windows-x64.exe
 #Invoke-WebRequest -Uri $url_bamboo -OutFile atlassian-bamboo-agent-installer-6.1.1.jar
 Invoke-WebRequest -Uri $url_git -OutFile gitinstall.exe
